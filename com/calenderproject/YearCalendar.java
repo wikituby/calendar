9025581520b525d0189d802a247d002a0a29683d
clyton tumwesigye <herdsontisk@gmail.com>
2023-04-11 17:12:58 +0300

    first ms

--- com/calenderproject/YearCalendar.java
+++ com/calenderproject/YearCalendar.java
@@ -49,17 +49,14 @@ public class YearCalendar {
         System.out.println("\n" + year + " Calendar\n");
         List<String> allHolidays = new ArrayList<>();
         for (int month = 0; month < 12; month++) {
-            // set the calendar to the first day of the month
             calendar.set(Calendar.MONTH, month);
             calendar.set(Calendar.DAY_OF_MONTH, 1);
             int weekday = calendar.get(Calendar.DAY_OF_WEEK);
 
-            // print out the month name and weekday headers
             String monthName = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
             System.out.println(monthName);
             System.out.println("Su Mo Tu We Th Fr Sa");
 
-            // print out the days of the month
             int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
             int day = 1;
             List<String> holidays = new ArrayList<>();
